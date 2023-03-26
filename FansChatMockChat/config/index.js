3bd0077e40abf199b4959a128ae136717db0aad9
XuChenMing <1187390456@qq.com>
2023-03-26 12:58:33 +0800

    1

--- FansChatMockChat/config/index.js
+++ FansChatMockChat/config/index.js
@@ -1,10 +1,10 @@
 let baseURL, browerUrl;
 if (process.env.VUE_APP_MODE === "dev") {
   console.log("dev");
-  baseURL = "https://fans-chat-prod-api.kr-cell.com/"; //
+  baseURL = "https://fans-chat-prod-api.kr-cell.com/"; // 正式
 } else if (process.env.VUE_APP_MODE === "test") {
   console.log("test");
-  baseURL = "http://47.254.172.49/"; // 正式 http://47.254.172.49
+  baseURL = "http://47.254.172.49/"; // 测试 http://47.254.172.49
 } else if (process.env.VUE_APP_MODE === "stage") {
   console.log("stage");
   baseURL = "";
