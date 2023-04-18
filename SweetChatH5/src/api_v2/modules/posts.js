ce63601dc5f98812272bfa53eb38c8e6f1ca5a98
yang5021 <t_001ing@163.com>
2023-04-18 20:53:11 +0800

    11

--- SweetChatH5/src/api_v2/modules/posts.js
+++ SweetChatH5/src/api_v2/modules/posts.js
@@ -10,26 +10,53 @@ export const postsList = (data) => {
 };
 // 点赞
 export const giveLike = (data) => {
-    return fetch({
-      url: "/api/dynamic/like",
-      method: "post",
-      data,
-    });
-  };
-  
-  // 取消点赞
-  export const cancelLike = (data) => {
-    return fetch({
-      url: "/api/dynamic/cancel-like",
-      method: "post",
-      data,
-    });
-  };
-  // 发布动态
-  export const postDynamic = (data) => {
-    return fetch({
-      url: "/api/dynamic/post-dynamic",
-      method: "post",
-      data,
-    });
-  };
+  return fetch({
+    url: "/api/dynamic/like",
+    method: "post",
+    data,
+  });
+};
+
+// 取消点赞
+export const cancelLike = (data) => {
+  return fetch({
+    url: "/api/dynamic/cancel-like",
+    method: "post",
+    data,
+  });
+};
+// 发布动态
+export const postDynamic = (data) => {
+  return fetch({
+    url: "/api/dynamic/post-dynamic",
+    method: "post",
+    data,
+  });
+};
+
+// 动态评论列表
+export const postsReply = (data) => {
+  return fetch({
+    url: "/api/dynamic/dynamic-reply-list",
+    method: "post",
+    data,
+  });
+};
+
+// 评论回复列表 -- 楼中楼
+export const postsToReply = (data) => {
+  return fetch({
+    url: "/api/dynamic/to-reply-list",
+    method: "post",
+    data,
+  });
+};
+
+// 对一级评论或楼中楼的回复
+export const postsReplyToReply = (data) => {
+  return fetch({
+    url: "/api/dynamic/reply-to-reply",
+    method: "post",
+    data,
+  });
+};
